304df59952215397d917061c09754b5c8d12b756
Jan Pucilowski <jan.pucilowski99@gmail.com>
2024-01-18 15:42:35 +0100

    fix: secretkey in bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,7 @@
 sudo -v
 
 read -p "Enter 1password email: " email
-read -p "Enter 1password secret key: " secret-key
+read -p "Enter 1password secret key: " secretkey
 
 NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
@@ -9,7 +9,7 @@ eval "$(/opt/homebrew/bin/brew shellenv)"
 
 brew install --cask 1password-cli
 
-eval $(op account add --address my.1password.com --email $email --secret-key $secret-key
+eval $(op account add --address my.1password.com --email $email --secret-key $secretkey
 
 brew install chezmoi
 
