a311fc32fa97b43ef3019b47b9584abecfb950c8
Jan Pucilowski <jan.pucilowski99@gmail.com>
2024-01-18 20:44:28 +0100

    feat: move dock order to a separate script

---
+++ run_onchange_dock-order.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Order Dock icons
+dockutil --remove all --no-restart
+dockutil --position 1 --add '/System/Applications/Launchpad.app' --section apps --no-restart
+dockutil --position 2 --add '/System/Applications/System Settings.app' --section apps --no-restart
+dockutil --position 3 --add '/System/Applications/Music.app' --section apps --no-restart
+dockutil --position 4 --add '/Applications/Arc.app' --section apps --no-restart
+dockutil --position 5 --add '/Applications/Messenger.app' --section apps --no-restart
+dockutil --position 6 --add '/Applications/Microsoft Teams (work or school).app' --section apps --no-restart
+dockutil --position 7 --add '/Applications/Visual Studio Code.app' --section apps --no-restart
+dockutil --position 8 --add '/Applications/Warp.app' --section apps --no-restart
+dockutil --position 0 --add '~/Downloads/' --section others --display stack --view fan
