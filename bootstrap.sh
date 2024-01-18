1b361944ad0db8416c9fc8d4eba982dc8c1c7772
Jan Pucilowski <jan.pucilowski99@gmail.com>
2024-01-18 15:28:35 +0100

    feat: extend bootstrap.sh script

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,1 +1,16 @@
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+sudo -v
+
+read -p "Enter 1password email: " email
+read -p "Enter 1password secret key: " secret-key
+
+NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+eval "$(/opt/homebrew/bin/brew shellenv)"
+
+brew install --cask 1password-cli
+
+eval $(op account add --address my.1password.com --email $email --secret-key $secret-key
+
+brew install chezmoi
+
+chezmoi init --apply https://github.com/janpuc/dotfiles.git
